f323367dfeb285686d7b1aa287a367fa913c7951
Aaron <supermanaaronliu@gmail.com>
2017-01-22 18:49:40 -0700

    done

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #  coding: utf-8 
 import SocketServer
-
+import os
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,11 +28,85 @@ import SocketServer
 
 
 class MyWebServer(SocketServer.BaseRequestHandler):
-    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+        command = self.data.split('\n')[0] # first line of the self.data
+        print(command+"\r\n")
+        (status, path, mime) = self.validateCommand(command)
+        self.send(status,path, mime)
+
+    def validateCommand(self, command_line):
+        """
+        validate the first line, return a status code, a path, and a mime type
+        - 404: path not found
+        - 405: Method Not Allowed
+        - 200: OK
+        """
+        tokens = command_line.split(' ')
+        if tokens[0].upper() != "GET": # method not allowed
+            return (405,None, None)
+        else:
+            if tokens[1] == "/":
+                return (200, "./www/index.html", "text/html")
+            else:
+                path = "./www"
+                if tokens[1][-1] == "/": # if token ends with /
+                    path += tokens[1]+"index.html"
+                    if os.path.isfile(path):
+                        return (200, path, "text/html")
+                    else:
+                        return (404, None, None)
+
+                elif tokens[1][-4:] == ".css":
+                    path += tokens[1]
+                    if os.path.isfile(path):
+                        return (200, path, "text/css")
+                    else:
+                        return (404, None, None)
+
+                elif tokens[1][-5:] == ".html":
+                    path += tokens[1]
+                    if os.path.isfile(path):
+                        return (200, path, "text/html")
+                    else:
+                        return (404, None, None)
+
+                else:
+                    path += tokens[1]+"/index.html"
+                    if os.path.isfile(path):
+                        return (200, path, "text/html")
+                    else:
+                        return (404, None, None)
+
+    def send(self,status_code,path,mimetype):
+        if status_code == 404:
+            header = "HTTP/1.1 404 Not Found\r\n"
+            mime = "Content-Type: text/html\r\n\r\n"
+
+            # this html content is written by wachowicz (http://blog.wachowicz.eu/?p=256)
+            content = "<!DOCTYPE html>" \
+                      "<html>" \
+                      "<body>" \
+                      "<p>" \
+                      "Error 404: File not found" \
+                      "</p>" \
+                      "</body>" \
+                      "</html>\r\n"
+
+        elif status_code == 405:
+            header = "HTTP/1.1 405 Method Not Allowed\r\n"
+            mime=""
+            content = ""
+        elif status_code == 200:
+            header = "HTTP/1.1 200 OK\r\n"
+            mime = "Content-Type: %s\r\n\r\n" % mimetype
+            file = open(path)
+            content = file.read().strip()+"\r\n"
+            file.close()
+
+        print(header)
+        self.request.send(header+mime+content)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
